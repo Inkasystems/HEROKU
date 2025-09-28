cb8e4523b9c308d8836e7e5de1b427753a0a39d1
JM_si <70615058@continental.edu.pe>
2025-09-28 17:47:42 -0500

    SE APLICA EL CAMBIO 3

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "¡Hola desde Heroku!";
+        return "¡Hola desde Heroku!FADSHFJDSKAHFAJSDHFKJDSAHKJFSD";
     }
 }
